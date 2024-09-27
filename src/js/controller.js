b48672e306961d72507d89c448ea733e97ffab96
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2024-09-28 02:29:42 +0300

    Refactor MVC

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,7 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +15,18 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const controlRecipes = async function () {
+  const id = window.location.hash.slice(1);
+  if (!id) return;
+  // Load Spinner
+  recipeView.loadSpinner();
+  // Load Recipe
+  await model.loadRecipe(id);
+
+  // Render The Recipe
+  recipeView.render(model.state.recipe);
+};
+
+['load', 'hashchange'].forEach(event =>
+  window.addEventListener(event, showRecipe)
+);
